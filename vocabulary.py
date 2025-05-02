2b30ca356b401f8f9bc74eefb49600d19d754628
Rabanis <1123581321george@gmail.com>
2025-05-02 13:36:45 +0300

    a small script to create a file with the lemmas of the sample documents.

---
+++ vocabulary.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from os.path import exists
+from parsing_utils import parse_file
+import re
+
+
+sample_file = './sample_col.csv'
+path = "./sample_collection"
+
+
+if __name__ == "__main__":
+
+    if not exists(sample_file):
+        # create the sample_col.csv
+        df = pd.DataFrame.from_dict(parse_file(path))
+        df.to_csv(sample_file, index=False)
+
+    #load all texts into a string to parse
+    sample_df = pd.read_csv(sample_file, usecols= [2], skiprows= [1])
+
+    text_list = sample_df["excerpt"].to_list()
+    master_string = ' '.join(text_list)
+    #cleaned = re.sub(r"[,]", "", master_string)
+    print(sample_df)
+
+    store_path = "./lemmas/"
+    #with open("./lemmas/master_string.txt", "w") as f:
+    #    f.write(cleaned)
