377807a529f542c0bf5653e0edc0bcf29004912b
Rabanis <1123581321george@gmail.com>
2025-05-05 14:09:45 +0300

    fine tuning for the lemmas

--- vocabulary.py
+++ vocabulary.py
@@ -6,25 +6,41 @@ import re
 
 sample_file = './sample_col.csv'
 path = "./sample_collection"
-lemmas = "./lemmas/master_string.txt.with_Frog.wlt.txt"
+lemmas_path = "./lemmas/master_string.txt.with_Frog.wlt.txt"
 
-
-if __name__ == "__main__":
+def main():
 
     if not exists(sample_file):
         # create the sample_col.csv
         df = pd.DataFrame.from_dict(parse_file(path))
         df.to_csv(sample_file, index=False)
 
+    if not exists(lemmas_path):
+        print("There's no lemma file!\nPlease go to /lemmas and run\n[ python3 ../glem/glem/glem.py -f master_string.txt -v -s with_Frog ]")
+        return
     #load all texts into a string to parse
     sample_df = pd.read_csv(sample_file, usecols= [2], skiprows= [1])
 
     text_list = sample_df["excerpt"].to_list()
     master_string = ' '.join(text_list)
-    cleaned = re.sub(r"\,|\.+|\·|\”|\'", "", master_string)
+    cleaned = re.sub(r"\,|\.+|\·|\”|\'|\“|\(|\)", "", master_string)
     cleaned = re.sub(r"\s+", " ", cleaned)
-    print(cleaned)
 
     store_path = "./lemmas/"
     with open("./lemmas/master_string.txt", "w") as f:
         f.write(cleaned)
+
+    create_vocabulary(lemmas_path)
+
+def create_vocabulary(source):
+    # after creating the lemmas, create a list of unique lemmas to save
+    lemmas = pd.read_csv(source, header= None, skipfooter=13, sep= "\t", names= ["Word", "Lemma", "POS-tag"], engine= "python")
+
+    vocab = lemmas["Lemma"].drop_duplicates(ignore_index= True)
+
+    print(lemmas.sort_values(by= "Lemma", axis= 0))
+    print(vocab)
+
+
+if __name__ == "__main__":
+    main()
