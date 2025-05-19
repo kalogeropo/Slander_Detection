eaf79e9a64c099d14c77cc15615c853ada2f95dc
Rabanis <1123581321george@gmail.com>
2025-05-19 12:32:41 +0300

    corrected on previous changes for proper results (peepee poopoo for loop before)

--- vocabulary.py
+++ vocabulary.py
@@ -11,8 +11,7 @@ lemmas_path = "./lemmas/master_string.txt.with_Frog.wlt.txt"
 
 def main():
     
-    test()
-    return
+    #test()
 
     if not exists(sample_file):
         # create the sample_col.csv
@@ -27,9 +26,29 @@ def main():
 
     text_list = sample_df["excerpt"].to_list()
     master_string = ' '.join(text_list)
+    # remove punctuations and same value letters
     cleaned = re.sub(r"\,|\.+|\·|\”|\'|\“|\(|\)", "", master_string)
     cleaned = re.sub(r"\s+", " ", cleaned)
     cleaned = re.sub(r"ττ", "σσ", cleaned)
+
+    # some transformation for hanlding unexpected cases of Dotiki klisi
+    # used unicode characters for the substitution
+    cleaned = cleaned.split()    
+    for i, w in enumerate(cleaned):
+        match = re.search("ηι$|ῆι$|ᾶι$|ῶι$|ωι$", w)
+        if match:
+            if w[-2] == "ῆ":
+                cleaned[i] = re.sub("ῆι$", "\u1fc7", w)  # ῆι - ῇ
+            elif w[-2] == "ᾶ":
+                cleaned[i] = re.sub("ᾶι$", "\u1fb7", w)  # ᾶι - ᾷ
+            elif w[-2] == "ῶ":
+                cleaned[i] = re.sub("ῶι$", "\u1ff7", w)  # ῶι - ῷ
+            elif w[-2] == "ω":
+                cleaned[i] = re.sub("ωι$", "\u1ff3", w)  # ωι - ῳ
+            elif w[-2] == "η":
+                cleaned[i] = re.sub("ηι$", "\u1fc3", w)  # ηι - ῃ
+
+    cleaned = " ".join(cleaned)
 
 
     store_path = "./lemmas/"
