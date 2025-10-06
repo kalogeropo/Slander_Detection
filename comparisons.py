eb23db7bfe4a34ef2c1ea2bf51a99d223331e16e
Rabanis <1123581321george@gmail.com>
2025-10-06 18:18:19 +0300

    Minor correction to vocabulary.text_clean_up() + adding of some comments. Also started the comparisons.py and initialized building the simple bm25+ module

--- comparisons.py
+++ comparisons.py
@@ -1,9 +1,18 @@
 import pandas as pd
 from os.path import exists
 import vocabulary
+from rank_bm25 import BM25Plus # based on the paper of reference, outperforms simple bm25 in every corpus
 
 def main():
-    pass
+    bm25_1()
+
+def bm25_1():
+    documentDf = pd.read_csv(vocabulary.sample_file, usecols= ["excerpt"])
+    documentDf.dropna(inplace= True)
+    # apply text cleanup to each doc and convert to list od cleaned docs
+    docList = documentDf["excerpt"].apply(vocabulary.text_clean_up).to_list()
+
+    print(docList)
 
 
 if __name__ == "__main__":
