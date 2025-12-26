5d6a1340295c20ac555cd7e0ea8ceb502f49f792
Rabanis <1123581321george@gmail.com>
2025-12-26 14:51:01 +0200

    First implamentation of BERT and first embeddings. Also stored pickled dataframes of the embeddings and csv files for all texts under pickle folder.

--- comparisons.py
+++ comparisons.py
@@ -5,38 +5,77 @@ import vocabulary
 from transformers import AutoTokenizer, AutoModel
 import torch
 from rank_bm25 import BM25Plus, BM25L, BM25Okapi # based on the paper of reference, outperforms simple bm25 in every corpus
+#from flair.models import SequenceTagger
 
 def main():
     average = "average_similarity.csv"
     lemmas = "lemmas_match_bm25.csv"
     exact = "exact_match_bm25.csv"
 
-    text = vocabulary.text_clean_up("δεινόν ἐστιν ἡ διαβολία, ὅτι οὐδὲ τιμωρία τις κατ’ αὐτῶν γέγραπται ἐν τοῖς νόμοις, ὥσπερ τῶν κλεπτῶν, καίτοι ἄριστον ὂν κτῆμα τὴν φιλίαν κλέπτουσιν· ὥστε ἡ ὕβρις κακοῦργος οὖσα δικαιοτέρα ἐστὶ τῆς διαβολῆς διὰ τὸ μὴ ἀφανὴς εἶναι")
-    AG_BERT(text)
+    documentDf = pd.read_csv(vocabulary.sample_file, usecols= ["excerpt"])
+    documentDf.dropna(inplace= True)
+    # apply text cleanup to each doc and convert to list of cleaned docs
+    docList = documentDf["excerpt"].apply(vocabulary.text_clean_up).to_list()
+    AG_BERT(docList)
     #bm25_1("lemmas")
     #weighted_value()
 
+# Function to create embeddings with Ancient Greek Bert model of HuggingFace
+# Input: list of texts
+# Output: list of vectors of the [CLS] tokens of all texts
+# The function also creates and saves the dataframes with all embeddings 
+# of each text in a pickle format for future usage without the need of running the model again.
+def AG_BERT(docList: list):
 
-def AG_BERT(text: str):
+    os.makedirs("./pickles/text_embeddings", exist_ok= True)
+    os.makedirs("./pickles/embeddings_csvs", exist_ok= True)
+    path_to_save_dfs_as_pickles = "./pickles/text_embeddings/"
+    path_to_save_dfs_as_csvs = "./pickles/embeddings_csvs/"
+
     # README instructions to load the model
     model_name = "pranaydeeps/ancient-greek-bert"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    ## tagger = SequenceTagger.load('libr/final-model.pt')
     model = AutoModel.from_pretrained(model_name)
     model.eval()  # Set to evaluation mode
 
-    inputs = tokenizer(text, return_tensors="pt")
-    print(inputs)
+    # model is set, time to tokenize texts
+    for id, text in enumerate(docList):
+        inputs = tokenizer(text, return_tensors="pt")
+        tokens = tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
+        #print((inputs.input_ids[0]))
 
-    with torch.no_grad():
-        output = model(**inputs)
-        #print(output[0][0][0])
+        with torch.no_grad():
+            # get the embeddings from bert
+            output = model(**inputs)
+            #create the dataframe to store the values
+            emb_df = pd.DataFrame(columns= ["token_id",
+                                            "position",
+                                            "token",
+                                            "embedding_vec",
+                                            "sub-word"])
+            
+            for i, token in enumerate(inputs.input_ids[0]):
+                sub_word = False # if current token has ## in front, it's a subtoken. Mark for later.
+                if tokens[i][0] == "#": sub_word = True
+#                                 tensor value id -> int    |||   tensor -> list of floats
+                emb_df.loc[i] = [int(token), i+1, tokens[i], output.last_hidden_state[0, i].tolist(), sub_word]
+            print(emb_df)
+            emb_df.to_pickle(path_to_save_dfs_as_pickles + f"text_{id+1}_emb_dataFrame.pkl")
+            emb_df.to_csv(path_to_save_dfs_as_csvs + f"text_{id+1}_embeddings.csv", header= True)
+            #print(id, "\n", len(output.last_hidden_state[0]))
+
+# inputs.input_ids[0] == list of token ids
+# output.last_hidden_state == output tensor for the model, only interested for the first entry
+# output.last_hidden_state[0] == list of embeddings for each token
+# output.last_hidden_state[0, 0] == embedding for [CLS], sum of meaning in a vector
+
 
 
 def bm25_1(mode: str):
 # we can pickle the splitted documents and append to it each time we want to add
 # a new document in the corpus but the training has to be redone each time with 
 # this bm25 library, we can't use existing vocabulary.
-
 
     documentDf = pd.read_csv(vocabulary.sample_file, usecols= ["excerpt"])
     documentDf.dropna(inplace= True)
